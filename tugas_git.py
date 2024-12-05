4a0b436cf6791b712a4ce995d3f51ff1758b83e9
ilham <ilham.rizky@mhs.itenas.ac.id>
2024-12-05 11:58:50 +0700

    versi 2

--- tugas_git.py
+++ tugas_git.py
@@ -41,4 +41,42 @@ data_panen = {
     }
 }
 
+# 1. Tampilkan seluruh data dari data_panen termasuk nama lokasi dan hasil panen masing-masing.
+for lokasi, data in data_panen.items():
+    print(f"Lokasi: {data['nama_lokasi']}")
+    print("Hasil Panen:")
+    for komoditas, hasil in data['hasil_panen'].items():
+        print(f"- {komoditas}: {hasil}")
+    print()  
+
+# 2. Tampilkan jumlah hasil panen jagung dari lokasi2
+jumlah_jagung_lokasi2 = data_panen['lokasi2']['hasil_panen']['jagung']
+print(f"*Jumlah hasil panen jagung di lokasi2: {jumlah_jagung_lokasi2}")
+
+# 3. Tampilkan nama lokasi dari lokasi3.
+nama_lokasi3 = data_panen['lokasi3']['nama_lokasi']
+print(f"*Nama lokasi 3: {nama_lokasi3}")
+
+# 4 & 5
+# 4. Masukkan Jumlah Hasil Panen Padi dan Kedelai Setiap Lokasi ke Dalam Variabel yang Berbeda:
+# 5. Buat variabel terpisah untuk menyimpan jumlah hasil panen padi dan kedelai dari setiap lokasi.
+hasil_padi = {}
+hasil_kedelai = {}
+
+for lokasi, data in data_panen.items():
+    hasil_padi[lokasi] = data['hasil_panen']['padi']
+    hasil_kedelai[lokasi] = data['hasil_panen']['kedelai']
+
+print("*Hasil Panen Padi:", hasil_padi)
+print("*Hasil Panen Kedelai:", hasil_kedelai)
+
+# 6. Buat Percabangan Jika jumlah hasil panen padi lebih dari 1300 atau jagung lebih dari 800 di suatu lokasi, maka lokasi tersebut memerlukan perhatian khusus. Jika tidak, maka lokasi tersebut dalam kondisi baik.
+for lokasi, data in data_panen.items():
+    padi = data['hasil_panen']['padi']
+    jagung = data['hasil_panen']['jagung']
+
+    if padi > 1300 or jagung > 800:
+        print(f" {lokasi} memerlukan perhatian khusus.")
+    else:
+        print(f" {lokasi} dalam kondisi baik.")
 
